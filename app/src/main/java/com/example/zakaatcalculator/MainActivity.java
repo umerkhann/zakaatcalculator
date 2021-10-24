6dc17f32d1e49687f7d10df86431cc719d09f387
Umer Khan <umeriskhan7@gmail.com>
2021-10-24 21:08:41 +0500

    Added variables

--- app/src/main/java/com/example/zakaatcalculator/MainActivity.java
+++ app/src/main/java/com/example/zakaatcalculator/MainActivity.java
@@ -2,12 +2,18 @@ package com.example.zakaatcalculator;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    private EditText cash, savings,loan, gold;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
 }
